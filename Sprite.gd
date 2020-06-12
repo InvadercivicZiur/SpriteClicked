dd419b3cdda709482f4ac9f5cfa8b040ee0d4dc1
InvadercivicZiur <47046970+InvadercivicZiur@users.noreply.github.com>
2020-06-12 02:03:18 -0700

    Update Sprite.gd

--- Sprite.gd
+++ Sprite.gd
@@ -16,26 +16,24 @@ func _input(event):
 	#good if you dont need to do something when the button is released
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if get_rect().has_point(get_local_mouse_position()):
+			get_tree().set_input_as_handled()
 			modulate = Color.blueviolet
 	
 	#if you need to do something when the buttons released
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(get_local_mouse_position()):
+			get_tree().set_input_as_handled()
 			if event.pressed:
 					modulate = Color.blueviolet
 			else:
-				modulate = Color.white
+				modulate = Color.red
 	
 	
 	#if you need to only select opague parts of the sprite replace get_rect()
 	#with is_pixel_opaque(get_local_mouse_position())
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if is_pixel_opaque(get_local_mouse_position()):
+			get_tree().set_input_as_handled()
 			modulate = Color.blueviolet
 	
-	
-	
-
-
-
 
