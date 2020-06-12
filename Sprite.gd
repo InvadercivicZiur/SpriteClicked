dc010210cdf4cf1802c26e2b04d13b644684141d
InvadercivicZiur <47046970+InvadercivicZiur@users.noreply.github.com>
2020-06-12 01:57:21 -0700

    Update Sprite.gd

--- Sprite.gd
+++ Sprite.gd
@@ -1,15 +1,37 @@
 extends Sprite
 
 
+
+
 func _input(event):
 	
-	if Input.is_mouse_button_pressed(1):
+	#select parts of the code and hold ctrl+k to comment it out
+	
+	#OG code. It works but there are more then 2 ways to skin a cat.
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if get_rect().has_point(to_local(event.position)):
+			print("A click!")
+	
+	
+	#good if you dont need to do something when the button is released
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if get_rect().has_point(get_local_mouse_position()):
-			if is_pixel_opaque(get_local_mouse_position()):
-				get_tree().set_input_as_handled()
-				modulate = Color.blueviolet
-	else:
-		modulate = Color.white
+			modulate = Color.blueviolet
+	
+	#if you need to do something when the buttons released
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if get_rect().has_point(get_local_mouse_position()):
+			if event.pressed:
+					modulate = Color.blueviolet
+			else:
+				modulate = Color.white
+	
+	
+	#if you need to only select opague parts of the sprite replace get_rect()
+	#with is_pixel_opaque(get_local_mouse_position())
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		if is_pixel_opaque(get_local_mouse_position()):
+			modulate = Color.blueviolet
 	
 	
 	
